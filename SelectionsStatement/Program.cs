bb1e9f3c9af902b827a210901c35648faac087e1
Craig McFarland <craigtmcfarland@gmail.com>
2020-10-26 18:35:50 -0400

    finish

--- SelectionsStatement/Program.cs
+++ SelectionsStatement/Program.cs
@@ -6,7 +6,40 @@ namespace SelectionsStatement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to my first number game!");
+            Console.WriteLine("See if you can guess my favorite number!");
+            Console.WriteLine("What is the upper limit for th erange of number");
+
+            var userResponse = Console.ReadLine();
+
+            var upperLimit = int.Parse(userResponse);
+
+
+            var random = new Random();
+            var number = random.Next(1, upperLimit);
+
+            Console.WriteLine("What is your Guess");
+            var guess = int.Parse(Console.ReadLine());
+
+            if (guess > number)
+            {
+                Console.WriteLine(" Your guess is too high");
+
+            }
+
+            if(guess < number)
+            {
+                Console.WriteLine("Your guess is too High");
+
+            }
+            else
+            {
+                Console.WriteLine("Correct");
+
+            }
+
+
+
         }
     }
 }
